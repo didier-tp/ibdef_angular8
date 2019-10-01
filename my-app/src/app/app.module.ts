173847f75842f538679b899aaf896e32f5b1007a
didier-tp <didier@d-defrance.fr>
2019-10-01 10:44:41 +0200

    calculatrice

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BasicComponent } from './basic/basic.component';
+import { CalculatriceComponent } from './basic/calculatrice/calculatrice.component';
+import { TvaComponent } from './basic/tva/tva.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BasicComponent,
+    CalculatriceComponent,
+    TvaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule ,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ my-app/src/app/basic/calculatrice/calculatrice.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculatrice',
+  templateUrl: './calculatrice.component.html',
+  styleUrls: ['./calculatrice.component.scss']
+})
+export class CalculatriceComponent implements OnInit {
+
+  x : number = 3;
+  y : number = 2;
+  res : number;
+
+  onAddition(){
+    this.res = Number(this.x)+Number(this.y);
+  }
+
+  onMultiplication(){
+    this.res = this.x*this.y;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
