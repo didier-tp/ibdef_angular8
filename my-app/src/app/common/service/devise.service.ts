3e17a2f029332b3de209ba99f23e079983947b3f
didier-tp <didier@d-defrance.fr>
2019-10-02 15:59:40 +0200

    s

--- my-app/src/app/common/service/devise.service.ts
+++ my-app/src/app/common/service/devise.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Devise } from '../data/devise';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeviseService {
+
+  //tableau temporaire (pour simulation):
+  private tabDevises = [
+    { code : 'EUR' , name : 'Euro' , change : 0.90} ,
+    { code : 'USD' , name : 'Dollar' , change : 1},
+    { code : 'GBP' , name : 'Livre' , change : 0.8} ,
+    { code : 'JPY' , name : 'Yen' , change : 120}
+  ]
+
+  //public getDevises() : Observable<Devise[]> {
+  public getDevises() : Devise[] {
+    return this.tabDevises;
+  }
+
+  //public convertir(...) Observable<number>{
+  public convertir(montant:number, codeSource : string , codeCible: string) : number{
+    return 1;
+  }
 
   constructor() { }
 }
--- my-app/src/app/conversion/conversion.component.ts
+++ my-app/src/app/conversion/conversion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DeviseService } from '../common/service/devise.service';
+import { Devise } from '../common/data/devise';
 
 @Component({
   selector: 'app-conversion',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conversion.component.scss']
 })
 export class ConversionComponent implements OnInit {
+  montant : number = 200;
+  codeMonnaieSource : string = 'EUR';
+  codeMonnaieCible : string = 'EUR';
+  montantConverti : number ;
+  listeDevises : Devise[];
 
-  constructor() { }
+  onConvertir(){
+    this.montantConverti =
+    this.deviseService.convertir(this.montant, 
+                                this.codeMonnaieSource, 
+                                this.codeMonnaieCible);
+  }
 
   ngOnInit() {
+    this.listeDevises = this.deviseService.getDevises();
   }
+
+  constructor(private deviseService : DeviseService) { 
+
+  }
+
+  
 
 }
