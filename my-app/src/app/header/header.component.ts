2e93d106fa9fedd9c6c644ac6c4e156e2ae800c0
didier-tp <didier@d-defrance.fr>
2019-10-04 11:24:49 +0200

    avec subscribe sur le header

--- my-app/src/app/header/header.component.ts
+++ my-app/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { PreferencesService } from '../common/service/preferences.service';
+import { OnlineOfflineService } from '../common/service/online-offline.service';
 
 @Component({
   selector: 'app-header',
@@ -15,13 +16,19 @@ export class HeaderComponent implements OnInit , OnChanges {
   @Input()
   titre : string = "default title";
 
-  constructor(public preferencesService : PreferencesService) {
+  constructor(public preferencesService : PreferencesService,
+              public onlineOfflineService : OnlineOfflineService) {
     //injection de dépendance automatique de this.preferencesService
     console.log("constructeur de HeaderComponent appelé");
    }
 
+   textColor : string = "blue"; //blue si onLine , red si "offLine"
+
   ngOnInit() {
     console.log("ngOnInit appelé");
+    this.onlineOfflineService.connectionChanged
+        .subscribe( (onLine)=>{ if(onLine) this.textColor = "blue";
+                                  else this.textColor = "red" ;})
   }
 
 }
